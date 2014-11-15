8b81bd3cdf86fd0ca3203fe980f100c8dc55672f
ersin <ersin_the_ertan@hotmail.com>
2014-11-14 23:57:56 -0500

    Chapter 4 - update table, start using content resolver in activity

--- chapter4/src/main/java/com/nullcognition/chapter4/ActivityMain.java
+++ chapter4/src/main/java/com/nullcognition/chapter4/ActivityMain.java
@@ -12,6 +12,52 @@ public class ActivityMain extends Activity {
   protected void onCreate(Bundle savedInstanceState){
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	android.content.ContentResolver cr = getContentResolver();
+	android.content.ContentValues contentValue = new android.content.ContentValues();
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_NAME, "Jason Wei");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_STATE, "CA");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_INCOME, 100000);
+	cr.insert(TableCitizen.CONTENT_URI, contentValue);
+	contentValue = new android.content.ContentValues();
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_NAME, "James Lee");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_STATE, "NY");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_INCOME, 120000);
+	cr.insert(TableCitizen.CONTENT_URI, contentValue);
+	contentValue = new android.content.ContentValues();
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_NAME, "Daniel Lee");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_STATE, "NY");
+	contentValue.put(com.nullcognition.chapter4.TableCitizen.COL_INCOME, 80000);
+	cr.insert(TableCitizen.CONTENT_URI, contentValue);
+// QUERY TABLE FOR ALL COLUMNS AND ROWS
+	android.database.Cursor c = cr.query(TableCitizen.CONTENT_URI, null, null, null, com.nullcognition.chapter4.TableCitizen.COL_INCOME + " ASC");
+// LET THE ACTIVITY MANAGE THE CURSOR
+	startManagingCursor(c);
+	int idCol = c.getColumnIndex(com.nullcognition.chapter4.TableCitizen.COL_ID);
+	int nameCol = c.getColumnIndex(com.nullcognition.chapter4.TableCitizen.COL_NAME);
+	int stateCol = c.getColumnIndex(com.nullcognition.chapter4.TableCitizen.COL_STATE);
+	int incomeCol = c.getColumnIndex(com.nullcognition.chapter4.TableCitizen.COL_INCOME);
+	while(c.moveToNext()){
+	  int id = c.getInt(idCol);
+	  String name = c.getString(nameCol);
+	  String state = c.getString(stateCol);
+	  int income = c.getInt(incomeCol);
+	  System.out.println("RETRIEVED ||" + id + "||" + name +
+						 "||" + state + "||" + income);
+	}
+	System.out.println("-------------------------------");
+// QUERY BY A SPECIFIC ID
+	android.net.Uri myC = android.net.Uri.withAppendedPath(TableCitizen.CONTENT_URI, "2");
+	android.database.Cursor c1 = cr.query(myC, null, null, null, null);
+// LET THE ACTIVITY MANAGE THE CURSOR
+	startManagingCursor(c1);
+	while(c1.moveToNext()){
+	  int id = c1.getInt(idCol);
+	  String name = c1.getString(nameCol);
+	  String state = c1.getString(stateCol);
+	  int income = c1.getInt(incomeCol);
+	  System.out.println("RETRIEVED ||" + id + "||" + name +
+						 "||" + state + "||" + income);
+	}
   }
 
 
--- chapter4/src/main/java/com/nullcognition/chapter4/TableCitizen.java
+++ chapter4/src/main/java/com/nullcognition/chapter4/TableCitizen.java
@@ -15,12 +15,13 @@ public class TableCitizen {
   public static final android.net.Uri CONTENT_URI = android.net.Uri.parse("content://" + ContentProviderCitizen.AUTHORITY + "/citizen");
 
   // MIME TYPE FOR GROUP OF CITIZENS
-  public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.jwei512.citizen";
+  public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.nullcognition.citizen";
 
   // MIME TYPE FOR SINGLE CITIZEN
-  public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.jwei512.citizen";
+  public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.nullcognition.citizen";
 
   // RELATIVE POSITION OF CITIZEN SSID IN URI
   public static final int SSID_PATH_POSITION = 1;
+
 
 }
